4ec2e5243da254241e4511ea1378166560f2aabd
Fernando Sena <fernando.sena@mkdata.com.br>
2025-02-05 08:10:16 -0300

    refact: change TestRestTemplate to WebTestClient

--- src/test/java/com/github/io/fernandosenacruz/star_wars_planet_api/application/e2e/PlanetIT.java
+++ src/test/java/com/github/io/fernandosenacruz/star_wars_planet_api/application/e2e/PlanetIT.java
@@ -3,114 +3,116 @@ package com.github.io.fernandosenacruz.star_wars_planet_api.application.e2e;
 import com.github.io.fernandosenacruz.star_wars_planet_api.application.domain.Planet;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static com.github.io.fernandosenacruz.star_wars_planet_api.common.PlanetConstants.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+
 @ActiveProfiles("it")
 @Sql(scripts = {"/insert_many_planets.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = {"/truncate_planets.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) public class PlanetIT {
-    @Autowired private TestRestTemplate restTemplate;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) @AutoConfigureWebTestClient
+public class PlanetIT {
 
-    // TODO success cases
+    @Autowired
+    private WebTestClient webTestClient;
 
-    @Test public void createPlanet_ReturnsCreated() {
-        ResponseEntity<Planet> planet_sut = restTemplate.postForEntity("/planets", HOTH, Planet.class);
-
-        assertThat(planet_sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        assertThat(Objects.requireNonNull(planet_sut.getBody()).getId()).isNotNull();
-        assertThat(planet_sut.getBody().getName()).isEqualTo(HOTH.getName());
-        assertThat(planet_sut.getBody().getClimate()).isEqualTo(HOTH.getClimate());
-        assertThat(planet_sut.getBody().getTerrain()).isEqualTo(HOTH.getTerrain());
+    @Test
+    public void createPlanet_ReturnsCreated() {
+        webTestClient.post().uri("/planets").bodyValue(HOTH).exchange().expectStatus().isCreated()
+                     .expectBody(Planet.class).value(planet -> {
+                         assertThat(planet.getId()).isNotNull();
+                         assertThat(planet.getName()).isEqualTo(HOTH.getName());
+                         assertThat(planet.getClimate()).isEqualTo(HOTH.getClimate());
+                         assertThat(planet.getTerrain()).isEqualTo(HOTH.getTerrain());
+                     });
     }
 
-    @Test public void getPlanet_ReturnsPlanet() {
-        ResponseEntity<Planet> planet_sut = restTemplate.getForEntity("/planets/1", Planet.class);
-
-        assertThat(planet_sut.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(Objects.requireNonNull(planet_sut.getBody()).getId()).isNotNull();
-        assertThat(planet_sut.getBody().getName()).isEqualTo(PLANET.getName());
-        assertThat(planet_sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
-        assertThat(planet_sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    @Test
+    public void getPlanet_ReturnsPlanet() {
+        webTestClient.get().uri("/planets/1").exchange().expectStatus().isOk()
+                     .expectBody(Planet.class).value(planet -> {
+                         assertThat(planet.getId()).isNotNull();
+                         assertThat(planet.getName()).isEqualTo(PLANET.getName());
+                         assertThat(planet.getClimate()).isEqualTo(PLANET.getClimate());
+                         assertThat(planet.getTerrain()).isEqualTo(PLANET.getTerrain());
+                     });
     }
 
-    @Test public void getPlanetByName_ReturnsPlanet() {
-        ResponseEntity<Planet> planet_sut =
-                restTemplate.getForEntity("/planets/name/" + PLANET.getName(), Planet.class);
-
-        assertThat(planet_sut.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(Objects.requireNonNull(planet_sut.getBody()).getId()).isNotNull();
-        assertThat(planet_sut.getBody().getName()).isEqualTo(PLANET.getName());
-        assertThat(planet_sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
-        assertThat(planet_sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+    @Test
+    public void getPlanetByName_ReturnsPlanet() {
+        webTestClient.get().uri("/planets/name/{name}", PLANET.getName()).exchange().expectStatus().isOk()
+                     .expectBody(Planet.class).value(planet -> {
+                         assertThat(planet.getId()).isNotNull();
+                         assertThat(planet.getName()).isEqualTo(PLANET.getName());
+                         assertThat(planet.getClimate()).isEqualTo(PLANET.getClimate());
+                         assertThat(planet.getTerrain()).isEqualTo(PLANET.getTerrain());
+                     });
     }
 
-    @Test public void getPlanets_ReturnsAllPlanets() {
-        ResponseEntity<Planet[]> response = restTemplate.getForEntity("/planets", Planet[].class);
+    @Test
+    public void getPlanets_ReturnsAllPlanets() {
+        webTestClient.get().uri("/planets").exchange().expectStatus().isOk()
+                     .expectBodyList(Planet.class).value(planets -> {
+                         assertThat(planets).isNotNull();
+                         assertThat(planets.size()).isEqualTo(PLANETS.size());
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+                         List<Planet> normalizedPlanets = planets.stream()
+                                                                 .map(p -> new Planet(
+                                                                         null, p.getName(), p.getClimate(),
+                                                                         p.getTerrain()
+                                                                 )).toList();
 
-        List<Planet> planetList = Arrays.asList(Objects.requireNonNull(response.getBody()));
-
-        assertThat(planetList.stream().filter(Objects::nonNull).count()).isEqualTo(PLANETS.size()); assertThat(
-                planetList.stream()
-                          .map(p -> new Planet(null, p.getName(), p.getClimate(), p.getTerrain()))
-                          .collect(Collectors.toList())).isEqualTo(PLANETS);
+                         assertThat(normalizedPlanets).isEqualTo(PLANETS);
+                     });
     }
 
-    @Test public void getPlanetsByClimate_ReturnsPlanets() {
-        ResponseEntity<Planet[]> response =
-                restTemplate.getForEntity(
-                        "/planets?" + String.format("climate=%s", ALDERAAN.getClimate()),
-                        Planet[].class
-                );
+    @Test
+    public void getPlanetsByClimate_ReturnsPlanets() {
+        webTestClient.get().uri(uriBuilder -> uriBuilder.path("/planets")
+                                                        .queryParam("climate", ALDERAAN.getClimate())
+                                                        .build())
+                     .exchange().expectStatus().isOk()
+                     .expectBodyList(Planet.class).value(planets -> {
+                         List<Planet> expected = List.of(ALDERAAN, YAVINIV);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+                         assertThat(planets.size()).isEqualTo(expected.size());
 
-        List<Planet> planetList = Arrays.asList(Objects.requireNonNull(response.getBody()));
-        List<Planet> planetsByClimate = Arrays.asList(ALDERAAN, YAVINIV);
+                         List<Planet> normalizedPlanets = planets.stream()
+                                                                 .map(p -> new Planet(
+                                                                         null, p.getName(), p.getClimate(),
+                                                                         p.getTerrain()
+                                                                 )).toList();
 
-        assertThat(planetList.stream().filter(Objects::nonNull).count()).isEqualTo(planetsByClimate.size());
-        assertThat(planetList.stream()
-                             .map(p -> new Planet(null, p.getName(), p.getClimate(), p.getTerrain()))
-                             .collect(Collectors.toList())).isEqualTo(
-                planetsByClimate);
-
+                         assertThat(normalizedPlanets).isEqualTo(expected);
+                     });
     }
 
-    @Test public void getPlanetsByTerrain_ReturnsPlanets() {
-        ResponseEntity<Planet[]> response =
-                restTemplate.getForEntity(
-                        "/planets?" + String.format("terrain=%s", YAVINIV.getTerrain()),
-                        Planet[].class
-                );
+    @Test
+    public void getPlanetsByTerrain_ReturnsPlanets() {
+        webTestClient.get()
+                     .uri(uriBuilder -> uriBuilder.path("/planets")
+                                                  .queryParam("terrain", YAVINIV.getTerrain()).build())
+                     .exchange().expectStatus().isOk()
+                     .expectBodyList(Planet.class).value(planets -> {
+                         assertThat(planets.size()).isEqualTo(1);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+                         planets.get(0).setId(null);
 
-        List<Planet> planetList = Arrays.asList(Objects.requireNonNull(response.getBody()));
-
-        assertThat(planetList.stream().filter(Objects::nonNull).count()).isEqualTo(1);
-        assertThat(planetList.stream().map(p -> new Planet(null, p.getName(), p.getClimate(), p.getTerrain()))
-                             .collect(Collectors.toList())).isEqualTo(List.of(YAVINIV));
+                         assertThat(planets.get(0)).usingRecursiveComparison().isEqualTo(YAVINIV);
+                     });
     }
 
-    @Test public void removePlanet_ReturnsNoContent() {
-        ResponseEntity<Void> response =
-                restTemplate.exchange("/planets/1", HttpMethod.DELETE, null, Void.class);
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    @Test
+    public void removePlanet_ReturnsNoContent() {
+        webTestClient.delete().uri("/planets/1").exchange().expectStatus().isNoContent();
     }
 }
+
